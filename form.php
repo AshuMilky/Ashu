6ffa212ca6b2e70e7d64bb88332a40d4617f8861
AshuMilky <132334137+AshuMilky@users.noreply.github.com>
2023-05-08 22:02:34 +0530

    form

---
+++ form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="connect.php">
+<label>Name:
+<input type="text" name="name"><br><br>
+</label>
+<label>Email:
+ <input type="email" name="email"><br><br>
+</label>
+<label>Phoneno:
+ <input type="phoneno" name="phoneno"><br><br>
+</label>
+<label>address:
+  <input type="text" name="address"><br><br>
+</label> 
+<input type="submit" name="submit">       
+
+    
+</body>
+</html>
