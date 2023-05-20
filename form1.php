c1a87b83625b5c759fe8bc7e91f39868bd760ea0
AshuMilky <132334137+AshuMilky@users.noreply.github.com>
2023-05-20 18:13:15 +0530

    form1

---
+++ form1.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg sticky-top  " id="navbar">
+        <div class="container-fluid">
+            <a href="#"><img src="trips.jpg"width="80" height="100" ></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span><img src="" alt="" width="30px"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              
+             
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <b>Trips</b>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:aquamarine;">
+                  
+                  <li><a class="dropdown-item" href="https://www.meenakshiholidays.com/wp-content/uploads/2022/08/goa-tour-from-chnnai-by-flight-meenakshi-holidays-1.jpg"
+                  >Goa trip</a></li>
+                  
+                  <li><a class="dropdown-item" href="https://hikestravel.com/wp-content/uploads/2021/11/kerala.jpg">Kerala trip</a></li>
+                  <li><a class="dropdown-item" href="https://content.r9cdn.net/rimg/dimg/43/2c/fe92dfc3-lm-48454-167a97bd72f.jpg?width=1366&height=768&xhint=1488&yhint=1244&crop=true">Mysore</a></li>
+                 
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Admin Form</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student-creat.php"><b>User Form</b></a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+    </nav>
+  </div>
+    
+            </div>
+        </div>
+        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+<!-- Indicators/dots -->
+<div class="carousel-indicators">
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+</div>
+
+<!-- The slideshow/carousel -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="https://media.cntraveller.com/photos/61b1f4863c1570da15248580/16:9/w_3200,h_1800,c_limit/tour%20ops.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+  </div>
+  <div class="carousel-item">
+    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/6d/ef/27/caption.jpg?w=1200&h=-1&s=1&cx=1920&cy=1080&chk=v1_9cf3de15fd3c34a7250c" alt="Chicago" class="d-block" style="width:100%">
+  </div>
+  <div class="carousel-item">
+    <img src="https://i.pinimg.com/originals/c7/d8/00/c7d8008825a4a9d7396fb85d61bf90b5.jpg" alt="New York" class="d-block" style="width:100%">
+  </div>
+  <div class="carousel-item">
+    <img src="https://media1.popsugar-assets.com/files/thumbor/yWJsMBh-SsOSsoZmPusJ1jp90q0/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2014/02/17/838/n/1922441/f0bd144ded7b1bfd_shutterstock_89720368/i/Maldives.jpg"class="d-block" style="width:100%">
+  </div>
+  <div class="carousel-item">
+    <img src="https://img.freepik.com/free-photo/long-boat-blue-water-maya-bay-phi-phi-island-krabi-thailand_335224-895.jpg?w=2000"class="d-block" style="width:100%">
+  </div>
+</div>
+
+<!-- Left and right controls/icons -->
+<button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</button>
+</div>
+
+
+
+
+
+<div class="container-fluid mt-3">
+  
+</div>
+<center><h1>WELCOME TO TOUR TRIPS</h1></center>
+        <div class="row" style="margin-top:30px">
+            <div class="col-md-3 py-3 py-md-0">
+              <div class="card">
+                <img src="https://hikestravel.com/wp-content/uploads/2021/11/kerala.jpg" alt="" height="425">
+                <div class="card-body">
+                  <h3 class="text-center">Kerala</h3>
+                  <p class="text-center"></p>
+                  <div class="star text-center">
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+            
+          <div class="col-md-3 py-3 py-md-0">
+            <div class="card">
+              <img src="https://gos3.ibcdn.com/india-tamil-nadu-149339162619o.jpeg" alt="" height="425">
+              <div class="card-body">
+                <h3 class="text-center">Tamil Nadu</h3>
+                <p class="text-center">lorem ipsum dolor sit amet.</p>
+                <div class="star text-center">
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                  <i class="fa-solid fa-star checked"></i>
+                </div>
+                
+              </div>
+            </div>
+            </div>
+            <div class="col-md-3 py-3 py-md-0">
+              <div class="card">
+                <img src="https://rivieratours.in/thumbnail/The%20Royal%20City%20-%20Mysore%204.png" alt="" height="425">
+                <div class="card-body">
+                  <h3 class="text-center">Mysore</h3>
+                  <p class="text-center">lorem ipsum dolor sit amet.</p>
+                  <div class="star text-center">
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                    <i class="fa-solid fa-star checked"></i>
+                  </div>
+              
+                </div>
+              </div>
+              </div>
+              <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                  <img src="https://www.weekendthrill.com/wp-content/uploads/2021/02/Goa-tour.jpg" alt="" height="425">
+                  <div class="card-body">
+                    <h3 class="text-center">Goa</h3>
+                    <p class="text-center">lorem ipsum dolor sit amet.</p>
+                    <div class="star text-center">
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                    </div>
+                    
+                  </div>
+                </div>
+                </div>
+                <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                  <img src="https://s3-external-1.amazonaws.com/youvisit-img/Assets/img/locations/80866/1280_profile.jpg?v=1527639138" alt="" height="425">
+                  <div class="card-body">
+                    <h3 class="text-center">Paris</h3>
+                    <p class="text-center">lorem ipsum dolor sit amet.</p>
+                    <div class="star text-center">
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                    </div>
+                    
+                  </div>
+                </div>
+                </div>
+                <div class="col-md-3 py-3 py-md-0">
+                <div class="card">
+                  <img src="https://cdn1.tripoto.com/media/filter/tst/img/2176903/Image/1632914414_18.jpg.webp" alt="" height="425">
+                  <div class="card-body">
+                    <h3 class="text-center">Swizerland</h3>
+                    <p class="text-center">lorem ipsum dolor sit amet.</p>
+                    <div class="star text-center">
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                      <i class="fa-solid fa-star checked"></i>
+                    </div>
+                    
+                  </div>
+                </div>
+                </div>
+              
+                
+          </div>
+        </div>
+</body>
+</html>
